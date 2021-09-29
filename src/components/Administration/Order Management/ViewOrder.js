af32d2ef1cee83eecfcf6e47dd7f127d0b7a5ff3
Kethaka-fonseka <kethaka.fsk@gmail.com>
2021-09-30 00:30:21 +0530

    Create ViewOrder.js file

--- src/components/Administration/Order Management/ViewOrder.js
+++ src/components/Administration/Order Management/ViewOrder.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, Container, Form, Row, Col,Dropdown } from "react-bootstrap";
-
+import axios from 'axios';
 
 function ViewOrder(props) {
-  return <div></div>;
+  const [orders,setOrders]=useState('');
+  useEffect(()=>{
+axios.get(`http;//localhost:8070/orders`).then(res=>{
+setOrders(res.data);
+}).catch(err=>{
+  console.log("err=>"+err);
+})
+  },[3])
+  
+  
+  return (
+ <div>
+<h2>Order List</h2>
+{orders.map((order,map)=>{
+return(
+<div>
+<h1>{order.user}</h1>
+
+</div>
+
+);
+})}
+</div>
+
+  );
 }
 
 export default ViewOrder;
