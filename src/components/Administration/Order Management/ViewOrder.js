2fee62da5d0b18737ab502db25f4d9999de87e57
Kethaka-fonseka <kethaka.fsk@gmail.com>
2021-09-30 19:13:52 +0530

    added new functions and update the code

--- src/components/Administration/Order Management/ViewOrder.js
+++ src/components/Administration/Order Management/ViewOrder.js
@@ -1,30 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Container, Form, Row, Col,Dropdown } from "react-bootstrap";
 import axios from 'axios';
+import { useLocation } from 'react-router';
+import ChangeStatus from './ChangeStatus';
+
 
 function ViewOrder(props) {
-  const [orders,setOrders]=useState('');
-  useEffect(()=>{
-axios.get(`http;//localhost:8070/orders`).then(res=>{
-setOrders(res.data);
-}).catch(err=>{
-  console.log("err=>"+err);
-})
-  },[3])
-  
+
+const location=useLocation(); 
+const products=location.state.order.products;
+const [status,setStatus]=useState(location.state.order.status);
   
   return (
  <div>
-<h2>Order List</h2>
-{orders.map((order,map)=>{
-return(
-<div>
-<h1>{order.user}</h1>
+<h1>Order No:{location.state.order._id}</h1>
+<table class='table'>
+<thead class='thead-dark'>
+  <tr class='table-success'>
+    <th scope='col'>Product ID</th>
+    <th scope='col'>Title</th>
+    <th scope='col'>Size</th>
+    <th scope='col'>Qty</th>
+  </tr>
+</thead>
+<tbody>
+  
+{products.map((product,index)=>{
+  return(
+    <tr >
+    <td>
+    {product.product_id}
+    </td>
+    <td>
+    </td>
+    <td>
+    {product.title}
+    </td>
+    <td >
+    {product.size}
+    </td>
+    <td>
+        
+    {product.price}
+    </td>
+  
 
-</div>
+  </tr>
+  
+  );
+  })}
+ 
+</tbody>
+</table>
+<ChangeStatus status={status}/>
 
-);
-})}
 </div>
 
   );
